6bd8666643f355ceca662e7be19bf57be3438e69
Piyush Khandelwal <khandelwalpiyush02@gmail.com>
2024-02-25 00:25:02 +0530

    master gateway checking route added.

--- constants/message.constants.js
+++ constants/message.constants.js
@@ -1,4 +1,5 @@
 exports.MESSAGE = {
+    API_GATEWAY_WORKING: 'API GATEWAY IS WORKING.',
     TRANSLATION_SUCCESS: 'The data translated Successfully.',
     SERVER_ERROR: 'Something went wrong, we are working on it !!!',
     MINMUM_MAXIMUM_LENGTH_ERROR: 'Text must be between 3 and 2000 characters',
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const router = require('./routes/v1/index.js');
+const httpStatus = require('http-status');
+const { MESSAGE } = require('./constants/message.constants.js');
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(httpStatus.OK).json({message: MESSAGE.API_GATEWAY_WORKING});
+})
 
 app.use('/translate', router);
 
